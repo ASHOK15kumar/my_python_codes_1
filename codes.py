7168a82d5a2d188dcf837583477bb903c0f23cc0
linuxx <sirasaniashok15@gmail.com>
2026-04-29 22:59:54 +0530

    day9

--- codes.py
+++ codes.py
@@ -143,6 +143,45 @@
 # userinput=int(input('enter a num: '))
 # prime(userinput)
 
+# a=67
+# if a<1:
+#     print('not prime')
+# else:
+#     for i in range(2,a):
+#         if a % i == 0 :
+#             print('not prime')
+#             break
+#     else:
+#         print('prime')
+
+'''--------to find the range of prime numbers-----------'''
+# inr=10
+# ine=22
+# primes=[]
+# for j in range(inr,ine+1):
+#     isprime=True
+#     for i in range(2,int(j**0.5)+1):
+#         if j % i == 0 :
+#             isprime=False
+#             break
+#     if isprime==True:
+#         primes.append(j)
+# print(primes)
+
+# def find_primes(a,b):
+#     primes=[]
+#     for j in range(a,b+1):
+#         isprime=True
+#         for i in range(2,int(j**0.5)+1):
+#             if j % i == 0 :
+#                 isprime=False
+#                 break
+#         if isprime:
+#             primes.append(j)
+#     return primes
+# k=find_primes(10,20)
+# print(k)
+
 
 '''------------------------ simple interest -------------------'''
 
@@ -183,8 +222,219 @@
 # squares = [i ** 2 for i in range(10)]
 # print("Squares:", squares)
 
-'''------------------multiplication table------------------------------'''
-val=int(input('enter num: '))
-for i in range(1,11):
-    res=val*i
-    print(f"{val}*{i}={res}")
+'''-----------------------multiplication tables-------------------------'''
+
+# val=int(input('enter num: '))
+# for i in range(1,11):
+#     res=val*i
+#     print(f"{val}*{i}={res}")
+
+'''-----------------------Bubble sort------------------------------------'''
+
+# def bubble_sort(arr):
+#     n = len(arr)
+#     for i in range(n - 1):
+#         for j in range(0, n - i - 1):
+#             if arr[j] > arr[j + 1]:
+#                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
+# arr = [64, 34, 25, 12, 22, 11, 90]
+# bubble_sort(arr)
+# print("Sorted array:", arr)
+
+
+
+
+'''--------------------------------------------------------------------------you have some questions here----------------'''
+
+
+# def bubble_sort(num):
+#     le=len(num)
+#     for i in range(le-1):
+#         for j in range(0,le-i-1):
+#             if  num[j] > num[j+1]:
+#                 num[j+1],num[j]=num[j],num[j+1]
+#     return num
+
+# var=[3,4,32,6,0,9]
+# bubble_sort(var)
+# print('sorted array: ',var)
+
+
+
+# def bubble_sort(num):
+#     le=len(num)
+#     for i in range(le-1):
+#         for j in range(0,le-i):
+#             if num[i] > num[j+i]:
+#                 num[j+i],num[i]=num[i],num[j+i]
+
+#     return num
+
+# var=bubble_sort([3,4,32,6,0,9])
+# print('sorted array: ',var)
+
+'''------------------------------count vowels--------------------'''
+
+# str='aifjoeijf0j03i0ir04i04i'
+# vov='aeiouAEIOU'
+# cnt=0
+# for i in range(len(str)):
+#     if str[i] in vov:
+#         cnt +=1
+# print(cnt)
+
+
+
+# def vovels(str='uhiuhihuihuhkj'):
+#     vov='aeiouAEIOU'
+#     cnt=0
+#     for i in range(len(str)):
+#         if str[i] in vov:
+#             cnt +=1
+#     return cnt
+# k=vovels('lsajifij3f3h44 0q0i409')
+# print(k)
+
+'''-------------------------------lcm-----page no 8 question no 9 you have a doubt overe here-------------------------------------'''
+
+# def compute_lcm(x, y):
+#     if x == 0 or y == 0:
+#         return 0
+
+#     if x > y:
+#         greater = x
+#     else:
+#         greater = y
+
+#     while True:
+#         if greater % x == 0 and greater % y == 0:
+#             return greater
+#         greater += 1
+
+
+# num1 = int(input("Enter first number: "))
+# num2 = int(input("Enter second number: "))
+
+# print("LCM:", compute_lcm(num1, num2))
+
+
+'''--------------------------------------anagram-------------------------------'''
+# You need 2 strings. They’re just rearranged versions of each other. Same letters, same count.
+# night ↔ thing 
+# debit card ↔ bad credit → spaces don’t matter if you ignore them  
+# astronomer ↔ moon starer
+# Notice: Both words have identical letters. If you sort them, they become the same: eilnst = eilnst
+
+
+# input1=str(input('enter a string: '))
+# # input2=str(input('enter a string: '))
+
+# # rep=input1.replace(' ','')
+# # rep2=input2.replace(' ','')
+# input1.sort()
+# print (input1)
+
+
+# s = "python" 
+# sorted_s = "   ".join(sorted(s)) 
+# print(sorted_s)
+
+'''-------------------------area of rectangle--------------------------'''
+
+# class Rectangle:
+#     def __init__(self, length, width):
+#         self.length = length
+#         self.width = width
+#     def area(self):
+#         return self.length * self.width
+# length = float(input("Enter length of the rectangle: "))
+# width = float(input("Enter width of the rectangle: "))
+# rect = Rectangle(length, width)
+# print("Area of the rectangle:", rect.area())
+
+
+
+# def prime_num(num):
+#     if num>0:
+#         primes=[2]
+#         val=2
+#         for i in range(2,val+1):
+#             for j in range(2,num):
+#                 if val % j == 0 and val % 2 ==0:
+#                     val = val+1
+#                 else:
+#                     primes=primes.append(val)
+#                     val +=1    
+#         return primes
+
+# value=prime_num(10)
+# print(value)
+
+
+
+
+# def prime_num(num):
+#     if num < 2:
+#         return []
+    
+#     primes = []
+#     for val in range(2, num + 1):  # check each number from 2 to num
+#         is_prime = True
+#         for j in range(2, int(val**0.5) + 1):  # only check up to sqrt(val)
+#             if val % j == 0:
+#                 is_prime = False
+#                 break
+#         if is_prime:
+#             primes.append(val)
+#     return primes
+# value = prime_num(10)
+# print(value)  # [2, 3, 5, 7]
+
+
+
+# inr=69
+# if inr<1:
+#     print('not prime')
+# else:
+#     for i in range(2,int(inr**0.5)+1):
+#         if inr % i == 0 :
+#             print('not prime')
+#             break
+#     else:
+#         print('prime')
+
+'''----------------------------------FIND LCM BETWEEN 2 NUMS -----------------------------'''
+
+# x=6
+# y=8
+# if x==y:
+#     lcm=x
+# elif x > y :
+#     greater =x
+# else:
+#     greater=y
+# while True:
+#     if greater % y ==0 and greater % x ==0 :
+#         lcm =greater
+#         break
+#     greater +=1
+# print(lcm)
+
+def find_lcm(x,y):
+    if x==y:
+        lcm=x
+    elif x > y :
+        greater =x
+    else:
+        greater=y
+    while True:
+        if greater % y ==0 and greater % x ==0 :
+            lcm =greater
+            break
+        greater +=1
+    return lcm
+a=int(input('enter num: '))
+b=int(input('enter num: '))
+
+vals=find_lcm(a,b)
+print(vals)
